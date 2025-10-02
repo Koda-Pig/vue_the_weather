e547de7cbcf1a42c2e3c60798387dc4a899340a0
Josh Koter <josh.koter@gmail.com>
2025-10-02 23:27:49 +0200

    ...

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+  <div>
+    <h1>put something here</h1>
+  </div>
+</template>
