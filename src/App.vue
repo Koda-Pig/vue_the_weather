11ec48bd0a73c12093a237146cc15dd4001bfa5f
Josh Koter <josh.koter@gmail.com>
2025-10-03 14:51:52 +0200

    ...

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@ import Form from './components/Form.vue'
 </script>
 
 <template>
-  <h1 class="sr-only">Vue the Weather</h1>
-
   <main class="p-4 md:p-8">
     <Form />
   </main>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,7 +2,11 @@
 // const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 import { ref } from 'vue'
 import Button from './ui/button/Button.vue'
+import Label from './ui/label/Label.vue'
+import Input from './ui/input/Input.vue'
 import Card from './ui/card/Card.vue'
+import CardContent from './ui/card/CardContent.vue'
+import CardFooter from './ui/card/CardFooter.vue'
 
 const openweatherKey = import.meta.env.VITE_APP_OPENWEATHER_KEY
 const geoapifyKey = import.meta.env.VITE_APP_GEOAPIFY_KEY
@@ -30,7 +34,7 @@ function error(err: GeolocationPositionError) {
   console.warn(`ERROR(${err.code}): ${err.message}`)
 }
 
-function handleSubmit() {
+function getDeviceLocation() {
   navigator.geolocation.getCurrentPosition(success, error, options)
 }
 
@@ -39,15 +43,41 @@ console.log(geoapifyKey)
 </script>
 
 <template>
-  <Card>
-    <form
-      @submit.prevent="handleSubmit"
-      class="border border-white rounded-xs p-4 max-w-[60ch] mx-auto bg-[rgba(255,255,255,0.05)]"
-    >
-      <input type="text" name="city" id="city" />
+  <Card class="w-[60ch] mx-auto text-center">
+    <h1 class="text-xl">Vue Weather</h1>
+    <CardContent class="">
       <!-- // this should have auto predict/ dropdown thing -->
-      <label for="city">location name</label>
-      <Button class="mx-auto block">get location</Button>
-    </form>
+      <Button size="lg" class="block mx-auto" @click="getDeviceLocation"
+        >get device location</Button
+      >
+
+      <p class="my-4 after:content-[''] after:h-0.5">or</p>
+
+      <div>
+        <Label for="location-name" class="block my-4">search location</Label>
+        <Input
+          type="text"
+          name="location-name"
+          id="location-name"
+          placeholder="location name"
+          class="text-center"
+        />
+
+        <!-- <Select>
+              <SelectTrigger id="framework">
+                <SelectValue placeholder="Select" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="nuxt"> Nuxt </SelectItem>
+                <SelectItem value="next"> Next.js </SelectItem>
+                <SelectItem value="sveltekit"> SvelteKit </SelectItem>
+                <SelectItem value="astro"> Astro </SelectItem>
+              </SelectContent>
+            </Select> -->
+      </div>
+    </CardContent>
+    <CardFooter>
+      <Button class="block mx-auto" size="lg">search location Name</Button>
+    </CardFooter>
   </Card>
 </template>
