9a8f736a749b00183295a3028ca9139ac444ec94
Josh Koter <josh.koter@gmail.com>
2025-10-03 13:16:38 +0200

    ...

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,11 +1,43 @@
 <script setup lang="ts">
 // const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 import { ref } from 'vue'
+const apiKey = import.meta.env.VITE_APP_OPENWEATHER_KEY
+const formData = ref({
+  city: '',
+})
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+}
+
+function success(pos: GeolocationPosition) {
+  const crd = pos.coords
+
+  console.log('Your current position is:')
+  console.log(`Latitude : ${crd.latitude}`)
+  console.log(`Longitude: ${crd.longitude}`)
+  console.log(`More or less ${crd.accuracy} meters.`)
+}
+
+function error(err: GeolocationPositionError) {
+  console.warn(`ERROR(${err.code}): ${err.message}`)
+}
+
+function handleSubmit() {
+  navigator.geolocation.getCurrentPosition(success, error, options)
+}
+
+console.log(apiKey)
 </script>
 
 <template>
-  <form>
-    <button>vue weather</button>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" name="city" id="city" />
+    <!-- // this should have auto predict/ dropdown thing -->
+    <label for="city">location name</label>
+    <button>get location</button>
   </form>
 </template>
 
---
+++ src/funks.ts
@@ -0,0 +1,2 @@
+// need to fetch weather data here.
+// also get user location
