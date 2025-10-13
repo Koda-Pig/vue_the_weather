14450dcfa25fb6a0a5bff35a40e3bf8877fed812
Josh Koter <josh.koter@gmail.com>
2025-10-13 20:42:54 +0200

    ...

--- src/components/PredictiveInput.vue
+++ src/components/PredictiveInput.vue
@@ -20,13 +20,15 @@ const geoApifyKey = import.meta.env.VITE_APP_GEOAPIFY_KEY
 const locationText = ref('')
 const globalState = defineModel<FormState>('state')
 const coords = defineModel<Coords | null>('coords')
+const abortController = ref<AbortController | null>(null)
 const locationPrediction = ref<LocationPrediction | null>(null)
 const noResultsFound = ref(false)
 const predictionState = ref<BaseState | null>(null)
-const MIN_CHAR_INPUT = 3
+const MIN_CHAR_INPUT = 3 // 3 characters are minimum input for predictive location to function
 
 function handlePredictionClick(predictionCoords: Coords) {
-  coords.value = { lat: predictionCoords.lat, lon: predictionCoords.lon }
+  const { lat, lon } = predictionCoords
+  coords.value = { lat, lon }
 }
 
 async function getLocationPrediction() {
@@ -35,19 +37,39 @@ async function getLocationPrediction() {
     throw new Error('GeoApify key is not set')
   }
 
+  // Cancel previous request
+  if (abortController.value) {
+    abortController.value.abort()
+    abortController.value = null
+  }
+
+  if (locationText.value.trim().length < MIN_CHAR_INPUT) {
+    console.log('Cancel search.')
+    return
+  }
+
+  abortController.value = new AbortController()
+
+  predictionState.value = 'loading'
+  noResultsFound.value = false
+
   try {
     const response = await fetch(
       `https://api.geoapify.com/v1/geocode/autocomplete?text=${locationText.value}&apiKey=${geoApifyKey}`,
-      { method: 'GET' },
+      { method: 'GET', signal: abortController.value.signal },
     )
     const data = await response.json()
     locationPrediction.value = data
 
-    const noFeatures = locationPrediction.value?.features?.length === 0
+    const noFeatures = !!(locationPrediction.value?.features?.length === 0)
 
-    noResultsFound.value = noFeatures
     predictionState.value = 'success'
+    noResultsFound.value = noFeatures
   } catch (error: unknown) {
+    if (error instanceof DOMException && error.name === 'AbortError') {
+      console.log('abort fetch of new predictions')
+      return // Ignore aborted requests
+    }
     globalState.value = 'error'
     predictionState.value = 'error'
     throw new Error(
@@ -63,20 +85,28 @@ const debouncedGetLocationPrediction = debounce(getLocationPrediction, 1000)
 watch(locationText, (newVal) => {
   const val = newVal.trim()
   if (val.length > MIN_CHAR_INPUT) {
-    predictionState.value = 'loading'
-    noResultsFound.value = false
     debouncedGetLocationPrediction()
   } else {
+    if (abortController.value) abortController.value.abort()
     predictionState.value = null
     noResultsFound.value = false
     locationPrediction.value = null
   }
 })
+
+watch(predictionState, (newState) => {
+  console.info('-------------------------')
+  console.log('state', newState)
+  console.log('noResults', noResultsFound.value)
+  console.log('locationPrediction', locationPrediction.value)
+  console.log('locationText', locationText.value)
+  console.info('-------------------------')
+})
 </script>
 
 <template>
   <div class="relative">
-    <Combobox v-model="locationText">
+    <Combobox v-model="locationText" :ignore-filter="true">
       <ComboboxAnchor class="w-full text-center">
         <ComboboxInput
           id="location-input"
